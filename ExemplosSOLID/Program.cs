2cd23efa81fd6fe85a3290ce5b23bccb7f87898e
AlberttSantos <alberttsantos@cartaodetodos.com>
2019-07-11 16:09:37 -0300

    Add project files.

---
+++ ExemplosSOLID/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemplosSOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
