0c526ef9369153936366730379c2e7705159b547
Albertt <alberttaurelio@gmail.com>
2019-07-11 23:37:13 -0300

    Adicionado ISP e InjecaoDependencia

--- ExemplosSOLID/ISP.cs
+++ ExemplosSOLID/ISP.cs
@@ -1,13 +1,64 @@
 ﻿namespace RuimISP
 {
-    class ISP
+    interface IDesenvolvimentoTODOS
     {
+        void Programar();
+        void Testar();
+    }
+
+    class Desenvolvedores : IDesenvolvimentoTODOS
+    {
+        public void Programar()
+        {
+            // Programar e tomar café
+        }
+
+        public void Testar()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class Testadores : IDesenvolvimentoTODOS
+    {
+        public void Programar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Testar()
+        {
+            // Testar todos alterações
+        }
     }
 }
 
 namespace BomISP
 {
-    class ISP
+    interface IDesenvolvedoresTODOS
     {
+        void Programar();
+    }
+
+    interface ITestadoresTODOS
+    {
+        void Testar();
+    }
+
+    class Desenvolvedores : IProgramadoresTODOS
+    {
+        public void Programar()
+        {
+            // Programar e tomar café
+        }
+      
+    }
+
+    class Testadores : ITestadoresTODOS
+    {
+        public void Testar()
+        {
+            // Testar
+        }
     }
 }
